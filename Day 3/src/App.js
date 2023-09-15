955f69c4f168c3b799ee03864c0d1b4849a57620
Monisha2930 <144903592+Monisha2930@users.noreply.github.com>
2023-09-15 11:24:22 +0530

    Add files via upload

---
+++ Day 3/src/App.js
@@ -0,0 +1,11 @@
+  import React from 'react'
+  import LoginPage from './login'
+  function App() {
+    return (
+      <div>
+        <LoginPage/>
+      </div>
+    )
+  }
+  
+  export default App
