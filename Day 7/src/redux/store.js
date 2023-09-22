04716bbd6ecc8a73027949e4c3dbaa7c65abf5ed
Monisha2930 <144903592+Monisha2930@users.noreply.github.com>
2023-09-22 14:43:38 +0530

    Add files via upload

---
+++ Day 7/src/redux/store.js
@@ -0,0 +1,9 @@
+import cartReducers from './cartSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice'
+export default configureStore({
+    reducer :{
+        user:userReducer,
+        addItem:cartReducers,
+    }
+})
