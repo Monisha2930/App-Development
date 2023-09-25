71ccb9669f29db9846b373d556eab9e4a8fbac93
Monisha2930 <144903592+Monisha2930@users.noreply.github.com>
2023-09-25 15:12:35 +0530

    Add files via upload

---
+++ Day 8/src/datas/products.js
@@ -0,0 +1,35 @@
+import img1 from '../assets/imgs'
+
+export const SliderProducts=
+[
+    {
+        name:'gift',
+        detail:'new ar',
+        price:'3456',
+        img:img1
+    },
+    {
+        name:'gift',
+        detail:'new ar',
+        price:'3456',
+        img:img1
+    },
+    // {
+    //     name:'gift',
+    //     detail:'new ar',
+    //     price:'3456',
+    //     img:img1
+    // },
+    // {
+    //     name:'gift',
+    //     detail:'new ar',
+    //     price:'3456',
+    //     img:img1
+    // },
+    // {
+    //     name:'gift',
+    //     detail:'new ar',
+    //     price:'3456',
+    //     img:img1
+    // },
+]
