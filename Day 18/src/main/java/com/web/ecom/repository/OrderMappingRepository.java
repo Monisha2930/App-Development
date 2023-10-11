539952202fa3460144b5750f3938b73e82284616
Monisha2930 <144903592+Monisha2930@users.noreply.github.com>
2023-10-11 22:12:49 +0530

    Delete Day 18 directory

--- Day 18/src/main/java/com/web/ecom/repository/OrderMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.web.ecom.model.OrderMapping;
-
-public interface OrderMappingRepository extends JpaRepository<OrderMapping, Long> {
-
-}
